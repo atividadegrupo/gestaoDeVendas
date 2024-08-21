f49a7b3b9e6bc554b596aa4af8c2da48a3f66794
Paulo-galvao <paulogalvaoj21@gmail.com>
2024-08-21 20:49:58 -0300

    funcionário e fornecedor

--- index.js
+++ index.js
@@ -1,30 +1,91 @@
 const express = require('express');
-const setor = require('./controllers/setor.js');
 const app = express();
 const port = 3000;
 
+const setor = require('./controllers/setor.js');
+const funcionario = require('./controllers/funcionario.js');
+const fornecedor = require('./controllers/fornecedor.js');
+
 app.use(express.json());
 
-app.post('/vendas', (req, res) => {
+/*
+    SETOR  
+*/
+
+app.post('/vendas/setor', (req, res) => {
     const code = setor.store(req.body);
     res.status(code).json();
 });
 
-app.get('/vendas', (req, res) => {
+app.get('/vendas/setor', (req, res) => {
     res.json(setor.showAll());
 })
 
-app.get('/vendas/:id', (req, res) => {
+app.get('/vendas/setor/:id', (req, res) => {
     res.json(setor.findId(req.params.id));
 })
 
-app.put('/vendas/:id', (req, res) => {
+app.put('/vendas/setor/:id', (req, res) => {
     const code = setor.update(req.params.id, req.body);
     res.status(code).json();
 });
 
-app.delete('/vendas/:id', (req,res) => {
+app.delete('/vendas/setor/:id', (req,res) => {
     const code = setor.destroy(req.params.id, req.body);
+    res.status(code).json();
+});
+
+/*
+    FUNCIONARIO
+*/
+
+app.post('/vendas/funcionario', (req, res) => {
+    const code = funcionario.store(req.body);
+    res.status(code).json();
+});
+
+app.get('/vendas/funcionario', (req, res) => {
+    res.json(funcionario.showAll());
+})
+
+app.get('/vendas/funcionario/:id', (req, res) => {
+    res.json(funcionario.findId(req.params.id));
+})
+
+app.put('/vendas/funcionario/:id', (req, res) => {
+    const code = funcionario.update(req.params.id, req.body);
+    res.status(code).json();
+});
+
+app.delete('/vendas/funcionario/:id', (req,res) => {
+    const code = funcionario.destroy(req.params.id, req.body);
+    res.status(code).json();
+});
+
+/*
+    FORNECEDOR
+*/
+
+app.post('/vendas/fornecedor', (req, res) => {
+    const code = fornecedor.store(req.body);
+    res.status(code).json();
+});
+
+app.get('/vendas/fornecedor', (req, res) => {
+    res.json(fornecedor.showAll());
+})
+
+app.get('/vendas/fornecedor/:id', (req, res) => {
+    res.json(fornecedor.findId(req.params.id));
+})
+
+app.put('/vendas/fornecedor/:id', (req, res) => {
+    const code = fornecedor.update(req.params.id, req.body);
+    res.status(code).json();
+});
+
+app.delete('/vendas/fornecedor/:id', (req,res) => {
+    const code = fornecedor.destroy(req.params.id, req.body);
     res.status(code).json();
 });
 
